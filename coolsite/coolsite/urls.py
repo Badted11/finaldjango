99a223d8cb15423ba4e362a2a35060d05555dcf3
Badted11 <dragon454607@gmail.com>
2023-04-25 18:55:53 +0600

    3st

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -25,6 +25,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('captcha/', include('captcha.urls')),
     path('', include('weather.urls')),
 ]
 
